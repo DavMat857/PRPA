8ecb2f5c64debe4ab23827077b0e1a970e3f2644
DavMat857 <124212989+DavMat857@users.noreply.github.com>
2023-03-13 02:06:19 +0100

    Update practica1_extra.py

--- practica1/practica1_extra.py
+++ practica1/practica1_extra.py
@@ -1,123 +1,129 @@
 from multiprocessing import Process
-from multiprocessing import BoundedSemaphore
+from multiprocessing import BoundedSemaphore, Lock, Semaphore
 from multiprocessing import current_process
 from multiprocessing import Value, Array
 
 from time import sleep
-import random
+import random   
 
 N = 3 # Cantidad de números que queremos que genere cada proceso
 NPROD = 4 # Cantidad de procesos
-
-
 
 def delay(factor = 3): #Introduce un factor de retraso aleatorio
     sleep(random.random()/factor)
 
 # Devuelve el mínimo de los productores teniendo en cuenta que -1 es vacío
 def min_list(lista): 
+    return min([x for x in lista if x > 0])
 
-    return min([x for x in lista if x >= 0])
 
-
-def producir(valor, almacen_personal, generado):
-   # Introducimos un valor del proceso_j en su almacen_personal_j
+def producir(valor, almacen_personal, empty, non_empty):
    
+ # Introducimos un valor del proceso_j en su almacen_personal_j
     for i in range(N):
-       
+        empty.acquire()
+
         print(f"Producer {current_process().name} produciendo", flush=True)
         
         # Aumentamos el valor de la variable compartida asociada a un proceso
         valor.value += random.randint(3, 10)
-        delay(2)
+        delay()
+        
         almacen_personal[i] = valor.value
-        
-        print(f"Producer {current_process().name} almacenado {valor.value}", flush=True)
+        print(f"Producer {current_process().name} almacenado {valor.value}")
 
-       # Me indica cuántos valores han sido generados por el proceso
-        generado.value = i
-    
-    
-    print(f"Producer {current_process().name} finalizado", flush=True)
-    generado.value = N + 1
-    
+        non_empty.release()
+        delay()
         
-def consumir(almacen_final, index, almacenes_productores, posiciones, generados):
     
+    
+    print(f"Producer {current_process().name} finalizado")
+
+        
+def consumir(almacen_final, almacenes_productores, semaforos_empty, semaforos_non_empty, posiciones):
+    # Inicializamos el índice del almacén 
     almacen_index = 0
-    finalizado = BoundedSemaphore(NPROD) # para tener en cuenta el número de procesos finalizados
+    
+    finalizado = BoundedSemaphore(NPROD)
+    
+    for i in range(NPROD):
+        semaforos_non_empty[i].acquire()
+        semaforos_non_empty[i].release() 
+
     while True:
-        
-        condicion_seguir = all(pos <= n.value for pos, n in zip(posiciones, generados))
-        
-       
-        if condicion_seguir:    # Esta condicional sirve para comprobar si cumplimos las condiciones para tomar el mínimo
-            comparacion = [alm[i] if i<len(alm) else -1 for alm, i in zip(almacenes_productores, posiciones)]
-            
-            
-            #Esta condición indica si ya se ha llenado el almacén, y por tanto hemos acabado
-            if  finalizado.get_value() == 0:
-                break
-            
-            
-            minimo = min_list(comparacion) #Tomamos el mínimo dentro de la lista comparación
-            delay()
-            print("El mínimo de la lista comparacion es:" , minimo)
 
-            # Obtener el índice del mínimo
-            index.value = comparacion.index(minimo)
-            
-            # Insertar el mínimo en el array final
-            almacen_final[almacen_index] = minimo
+        if finalizado.get_value() == 0:
+            break 
 
-            # Actualizar el índice del array final
-            almacen_index += 1
-            
-            
-            posiciones[index.value] += 1
-            if posiciones[index.value] == N:
-                finalizado.acquire()
-            
-            print("Comparación actual:", comparacion[:])
-            print("Almacen por el momento", almacen_final[:])
-            
-        else: #Esperamos a que se genere un valor por cada proceso que podamos tomar
-            delay()
+        # Esperar a que todos los productores hayan producido al menos un valor
+
+        # Extraer elementos de los almacenes de los productores
+        comparacion = [alm[i] if i < len(alm) else -1 for alm, i in zip(almacenes_productores, posiciones)]
+        
+        # Escoger el mínimo de los valores producidos por los productores
+        print("Comparación actual:", comparacion[:])
+        minimo = min_list(comparacion)
+        delay()
+        print("El mínimo de la lista comparacion es:" , minimo)
+
+        # Obtener el índice del mínimo
+        index_minimo = comparacion.index(minimo)
+
+        semaforos_empty[index_minimo].release()  
+        
+        # Insertar el mínimo en el array final
+        almacen_final[almacen_index] = minimo
+
+        # Actualizar el índice del array final
+        almacen_index += 1
+        
+        # Actualizar el índice del almacen del productor correspondiente
+        posiciones[index_minimo] += 1
+
+        if posiciones[index_minimo] == N:
+            finalizado.acquire()
+
+        semaforos_non_empty[index_minimo].acquire()  
+        
+        print("Almacen por el momento", almacen_final[:])
+        delay()
         
 def main():
     
     almacen_final = Array('i', N * NPROD) # Almacenamiento de todos los valores
-    comparacion = Array('i', NPROD)    # Array que nos sirve para realizar comparaciones intermedias y tomar los valores
-    index = Value('i', 0)
-
-    almacenes_productores = [Array('i', N) for _ in range(NPROD)]
-    posiciones = Array('i', NPROD) # Es un array que contiene la última posición del almacen_productores introducido
+    posiciones = Array('i', NPROD) # Array que nos sirve para realizar comparaciones intermedias y tomar los valores
     
-    for i in range(NPROD):
-        comparacion[i] = -2
+
     
     for i in range(NPROD):
         posiciones[i] = 0
         
     for i in range(N * NPROD):
         almacen_final[i] = -2
+
+    semaforos_empty = [Lock() for _ in range(NPROD)]
+    semaforos_non_empty = [Semaphore(0) for _ in range(NPROD)]
         
     valores_iniciales = [Value('i', -2) for _ in range(NPROD)]
-    generados = [Value('i', -1) for _ in range(NPROD)] # Es un array que contiene cuanto elementos ha generado un productor
-    
+    almacenes_productores = [Array('i', N) for _ in range(NPROD)]
+
+    for b in almacenes_productores:
+        for i in range(N):
+            b[i] = -2
     
     productores = [Process(target=producir,
                               name=f'p_{i}',
-                              args=(valores_iniciales[i],            
+                              args=(valores_iniciales[i],
                                     almacenes_productores[i],
-                                    generados[i]))
+                                    semaforos_empty[i],
+                                    semaforos_non_empty[i]))
                      for i in range(NPROD)]
     
-    consumidor = Process(target=consumir, args=(almacen_final,
-                                                index,
-                                                almacenes_productores,
-                                                posiciones,
-                                                generados))
+    consumidor = Process(target=consumir, name="c", args=(almacen_final,
+                                                          almacenes_productores,
+                                                          semaforos_empty,
+                                                          semaforos_non_empty,
+                                                          posiciones))
     
     for p in productores + [consumidor]:
         p.start()
