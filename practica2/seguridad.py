066dde65e335f1f6454860c2d07b84080f1d817c
David <labrador.jin@gmail.com>
2023-03-23 19:43:28 +0100

    nuevos archivos: seguridad.py 	nuevos archivos: sin_inanicion.py

---
+++ practica2/seguridad.py
@@ -0,0 +1,241 @@
+"""
+OPCIÓN SEGURA
+
+Esta formaría parte de la opción segura, debido a que :
+
+Los coches del norte pueden pasar siempre que no haya coches del sur ni peatones
+
+Los coches del sur pueden pasar siempre que no haya coches del norte ni peatones
+
+Los peatones pueden pasar siempre que no hayan coches
+
+"""
+import time
+import random
+from multiprocessing import Lock, Condition, Process
+from multiprocessing import Value
+
+SOUTH = 1
+NORTH = 0
+
+NCARS = 20
+NPED = 10
+TIME_CARS_NORTH = 0.5  # a new car enters each 0.5s
+TIME_CARS_SOUTH = 0.5  # a new car enters each 0.5s
+TIME_PED = 5 # a new pedestrian enters each 5s
+TIME_IN_BRIDGE_CARS = (1, 0.5) # normal 1s, 0.5s
+TIME_IN_BRIDGE_PEDESTRIAN = (30, 10) # normal 1s, 0.5s
+
+class Monitor():
+    def __init__(self):
+        self.mutex = Lock() # Inicializa un candado (Lock) para proteger el acceso a las variables compartidas
+        self.patata = Value('i', 0) # Variable compartida sin uso específico en este código
+
+        # Número de coches y peatones
+        self.ncars_N = Value('i', 0) # Número de coches en dirección Norte
+        self.ncars_S = Value('i', 0) # Número de coches en dirección Sur
+        self.nped   = Value('i', 0) # Número de peatones
+
+        # Condiciones
+        self.can_cars_N=Condition(self.mutex) # Condición para controlar el paso de coches en dirección Norte
+        self.can_cars_S=Condition(self.mutex) # Condición para controlar el paso de coches en dirección Sur
+        self.can_ped  =Condition(self.mutex) # Condición para controlar el paso de peatones
+
+
+#############COCHES
+    # Método para solicitar entrada al puente para un coche
+    def wants_enter_car(self, direction: int) -> None:
+        self.mutex.acquire() # Adquiere el candado para proteger el acceso a las variables compartidas
+
+        # Incrementa el valor de la variable patata
+        self.patata.value += 1
+
+        # Si el coche va en dirección Norte
+        if direction == NORTH:
+            # Espera hasta que pueda pasar un coche en dirección Norte
+            self.can_cars_N.wait_for(self.can_pass_cars_N)
+            self.ncars_N.value +=1 # Incrementa el número de coches en dirección Norte
+
+        # Si el coche va en dirección Sur
+        else:
+            # Espera hasta que pueda pasar un coche en dirección Sur
+            self.can_cars_S.wait_for(self.can_pass_cars_S)
+            self.ncars_S.value+=1 # Incrementa el número de coches en dirección Sur
+
+        self.mutex.release() # Libera el candado
+
+    # Método para indicar que un coche sale del puente
+    def leaves_car(self, direction: int) -> None:
+        self.mutex.acquire() # Adquiere el candado para proteger el acceso a las variables compartidas
+        self.patata.value += 1 # Incrementa el valor de la variable patata
+
+        # Si el coche va en dirección Norte
+        if direction==NORTH:
+            self.ncars_N.value-=1 # Decrementa el número de coches en dirección Norte
+            # Si ya no hay coches en dirección Norte
+            if self.ncars_N.value==0:
+                # Notifica a coches en dirección Sur y peatones que pueden pasar
+                self.can_cars_S.notify_all()
+                self.can_ped.notify_all()
+
+        # Si el coche va en dirección Sur
+        else:
+            self.ncars_S.value-=1 # Decrementa el número de coches en dirección Sur
+            # Si ya no hay coches en dirección Sur
+            if self.ncars_S.value==0:
+                # Notifica a coches en dirección Norte y peatones que pueden pasar
+                self.can_cars_N.notify_all()
+                self.can_ped.notify_all()
+
+        self.mutex.release() # Libera el candado
+
+#############PEATONES
+    # Método para solicitar entrada al puente para un peatón
+    def wants_enter_pedestrian(self) -> None:
+        self.mutex.acquire() # Adquiere el candado para proteger el acceso a las variables compartidas
+        self.patata.value += 1 # Incrementa el valor de la variable patata
+
+        # Espera hasta que pueda pasar un peatón
+        self.can_ped.wait_for(self.can_pass_ped)
+        self.nped.value+=1 # Incrementa el número de peatones
+        self.mutex.release() # Libera el candado
+
+    # Método para indicar que un peatón sale del puente
+    def leaves_pedestrian(self) -> None:
+        self.mutex.acquire() # Adquiere el candado para proteger el acceso a las variables compartidas
+        self.patata.value += 1 # Incrementa el valor de la variable patata
+        self.nped.value-=1 # Decrementa el número de peatones
+
+        # Si ya no hay peatones
+        if self.nped.value==0:
+            # Notifica a coches en dirección Norte y Sur que pueden pasar
+            self.can_cars_N.notify_all()
+            self.can_cars_S.notify_all()
+
+        self.mutex.release() # Libera el candado
+
+    # Verifica si pueden pasar coches en dirección Sur
+    def can_pass_cars_S(self) -> bool:
+        # Retorna verdadero si no hay coches en dirección Norte ni peatones
+        return self.ncars_N.value == 0 and self.nped.value == 0
+
+    # Verifica si pueden pasar coches en dirección Norte
+    def can_pass_cars_N(self) -> bool:
+        # Retorna verdadero si no hay coches en dirección Sur ni peatones
+        return self.ncars_S.value == 0 and self.nped.value == 0
+
+    # Verifica si pueden pasar peatones
+    def can_pass_ped(self) -> bool:
+        # Retorna verdadero si no hay coches en dirección Norte ni en dirección Sur
+        return self.ncars_N.value == 0 and self.ncars_S.value == 0
+
+    # Método para representar el objeto como una cadena de texto
+    def __repr__(self) -> str:
+        # Retorna una cadena con el valor de la variable patata
+        return f'Monitor: {self.patata.value}'
+
+
+
+#RETRASOS
+# Función de retraso para coches en dirección Norte
+def delay_car_north(factor = 3) -> None:
+    time.sleep(random.random()/factor) # Espera un tiempo aleatorio dividido por el factor
+
+# Función de retraso para coches en dirección Sur
+def delay_car_south(factor = 3) -> None:
+    time.sleep(random.random()/factor) # Espera un tiempo aleatorio dividido por el factor
+
+# Función de retraso para peatones
+def delay_pedestrian(factor = 3) -> None:
+    time.sleep(random.random()/factor) # Espera un tiempo aleatorio dividido por el factor
+
+# Función para simular el paso de un coche por el puente
+def car(cid: int, direction: int, monitor: Monitor)  -> None:
+    # Muestra que el coche quiere entrar al puente
+    print(f"car {cid} heading {direction} wants to enter. {monitor}",flush=True)
+    monitor.wants_enter_car(direction) # Solicita entrada al puente
+    # Muestra que el coche entra al puente
+    print(f"car {cid} heading {direction} enters the bridge. {monitor}",flush=True)
+    
+    # Aplica un retraso según la dirección del coche
+    if direction==NORTH :
+        delay_car_north()
+    else:
+        delay_car_south()
+    
+    # Muestra que el coche está dejando el puente
+    print(f"car {cid} heading {direction} leaving the bridge. {monitor}",flush=True)
+    monitor.leaves_car(direction) # Indica que el coche sale del puente
+    # Muestra que el coche ha salido del puente
+    print(f"car {cid} heading {direction} out of the bridge. {monitor}",flush=True)
+
+# Función para simular el paso de un peatón por el puente
+def pedestrian(pid: int, monitor: Monitor) -> None:
+    # Muestra que el peatón quiere entrar al puente
+    print(f"pedestrian {pid} wants to enter. {monitor}",flush=True)
+    monitor.wants_enter_pedestrian() # Solicita entrada al puente
+    # Muestra que el peatón entra al puente
+    print(f"pedestrian {pid} enters the bridge. {monitor}",flush=True)
+    delay_pedestrian() # Aplica un retraso al peatón
+    # Muestra que el peatón está dejando el puente
+    print(f"pedestrian {pid} leaving the bridge. {monitor}",flush=True)
+    monitor.leaves_pedestrian() # Indica que el peatón sale del puente
+    # Muestra que el peatón ha salido del puente
+    print(f"pedestrian {pid} out of the bridge. {monitor}",flush=True)
+
+
+
+#Funciones de generación de peatones y coches
+
+# Función para generar peatones y simular su paso por el puente
+def gen_pedestrian(monitor: Monitor) -> None:
+    pid = 0 # Inicializa el identificador del peatón
+    plst = [] # Lista para almacenar los procesos de peatones
+    for _ in range(NPED): # Repite NPED veces
+        pid += 1 # Incrementa el identificador del peatón
+        # Crea un nuevo proceso para el peatón y lo inicia
+        p = Process(target=pedestrian, args=(pid, monitor))
+        p.start()
+        plst.append(p) # Añade el proceso a la lista
+        # Espera un tiempo aleatorio basado en una distribución exponencial
+        time.sleep(random.expovariate(1/TIME_PED))
+
+    for p in plst: # Espera a que todos los peatones terminen
+        p.join()
+
+# Función para generar coches en una dirección y simular su paso por el puente
+def gen_cars(direction: int, time_cars, monitor: Monitor) -> Monitor:
+    cid = 0 # Inicializa el identificador del coche
+    plst = [] # Lista para almacenar los procesos de coches
+    for _ in range(NCARS): # Repite NCARS veces
+        cid += 1 # Incrementa el identificador del coche
+        # Crea un nuevo proceso para el coche y lo inicia
+        p = Process(target=car, args=(cid, direction, monitor))
+        p.start()
+        plst.append(p) # Añade el proceso a la lista
+        # Espera un tiempo aleatorio basado en una distribución exponencial
+        time.sleep(random.expovariate(1/time_cars))
+
+    for p in plst: # Espera a que todos los coches terminen
+        p.join()
+
+# Función principal del programa
+def main():
+    monitor = Monitor() # Crea una instancia del monitor
+    # Crea procesos para generar coches en dirección norte y sur, y peatones
+    gcars_north = Process(target=gen_cars, args=(NORTH, TIME_CARS_NORTH, monitor))
+    gcars_south = Process(target=gen_cars, args=(SOUTH, TIME_CARS_SOUTH, monitor))
+    gped = Process(target=gen_pedestrian, args=(monitor,))
+    # Inicia los procesos
+    gcars_north.start()
+    gcars_south.start()
+    gped.start()
+    # Espera a que los procesos terminen
+    gcars_north.join()
+    gcars_south.join()
+    gped.join()
+    print("Se acabo el programa") # Imprime un mensaje cuando termina el programa
+
+if __name__ == '__main__':
+    main() # Llama a la función principal
+    
