5a9c5287bfd0dd9edd8e8f9db1f0f4b65a7ca513
DavMat857 <124212989+DavMat857@users.noreply.github.com>
2023-04-15 03:29:39 +0200

    Update 03_temperatures.py

--- Ejercicios_mqqt/03_temperatures.py
+++ Ejercicios_mqqt/03_temperatures.py
@@ -1,47 +1,51 @@
 # -*- coding: utf-8 -*-
-
-from threading import Lock #No estamos usando multiprocessing
-# en realidad aquí no hace falta el lock, solo hay 1 hebra en
-# ejecución.
+#python t2.py simba.fdi.ucm.es temperatures
+""" toma un diccionario de temperaturas para cada sensor y calcula la temperatura máxima, mínima y media para cada sensor y de todos los sensores. Luego, dentro del bucle principal, llamamos a esta función y mostramos las estadísticas calculadas por consola. Además, hemos modificado la lógica para procesar los datos recibidos del servidor MQTT para que los valores de temperatura se conviertan a float antes de ser agregados al diccionario de datos."""
 from paho.mqtt.client import Client
 from time import sleep
 
 def on_message(mqttc, data, msg):
-    print ('on_message', msg.topic, msg.payload)
+    print('on_message', msg.topic, msg.payload)
     n = len('temperature/')
-    lock = data['lock']
-    lock.acquire()
-    try:
-        key = msg.topic[n:]
-        if key in data:
-            data['temp'][key].append(msg.payload)
-        else:
-            data['temp'][key]=[msg.payload]
-    finally:
-        lock.release()
-    print ('on_message', data)
+    key = msg.topic[n:]
+    if key in data:
+        data['temp'][key].append(float(msg.payload))
+    else:
+        data['temp'][key] = [float(msg.payload)]
+    print('on_message', data)
 
+def calculate_statistics(data):
+    statistics = {'max': {}, 'min': {}, 'mean': {}}
+    for key, temp in data.items():
+        statistics['max'][key] = max(temp)
+        statistics['min'][key] = min(temp)
+        statistics['mean'][key] = sum(temp) / len(temp)
+    statistics['max_all'] = max(max_temp for max_temp in statistics['max'].values())
+    statistics['min_all'] = min(min_temp for min_temp in statistics['min'].values())
+    statistics['mean_all'] = sum(statistics['mean'].values()) / len(statistics['mean'])
+    return statistics
 
 def main(broker):
-    data  = {'lock':Lock(), 'temp':{}}
+    data = {'temp': {}}
     mqttc = Client(userdata=data)
     mqttc.on_message = on_message
     mqttc.connect(broker)
     mqttc.subscribe('temperature/#')
     mqttc.loop_start()
-
     while True:
-        sleep(8)
-        for key,temp  in data['temp'].items():
-            mean = sum(map(lambda x: int(x), temp))/len(temp)
-            print(f'mean {key}: {mean}')
-            data[key]=[]
-
-
+        sleep(5)
+        statistics = calculate_statistics(data['temp'])
+        print('Max temperature:', statistics['max_all'])
+        print('Min temperature:', statistics['min_all'])
+        print('Mean temperature:', statistics['mean_all'])
+        print('Per-sensor statistics:')
+        for key in sorted(data['temp'].keys()):
+            print(f'{key}: Max {statistics["max"][key]}, Min {statistics["min"][key]}, Mean {statistics["mean"][key]}')
+        data['temp'] = {}
 
 if __name__ == "__main__":
     import sys
-    if len(sys.argv)<2:
+    if len(sys.argv) < 2:
         print(f"Usage: {sys.argv[0]} broker")
         sys.exit(1)
     broker = sys.argv[1]
